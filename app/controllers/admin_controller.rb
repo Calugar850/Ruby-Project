a6d6ecf00ed2225d1a2904b8dac98f487254e852
Calugar850 <calugargabriel38@yahoo.com>
2022-07-15 17:45:54 +0300

    Don't save user create by admin

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,13 @@
+class AdminController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(friend_params)
+  end
+
+  def friend_params
+    params.require(:user).permit(:username, :name, :email, :password, :current_password, :domain, :role)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,26 @@
+require_relative "../lib/route_constraints/user_exists"
+
 Rails.application.routes.draw do
+  #get 'admin/new'
   devise_for :users
   #get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  #resources :user, param: :domain
+  root "home#index"
+
+  #scope "/:domain", constraints: RouteConstraints::UserExists do
+  #  get "/", to: "home#show"
+  #end
+  #get '/a', to: 'home#show', constraints: { subdomain: ':domain' }
+
+  constraints subdomain: 'utcn' do
+    get '/dashboard', to: 'home#show'
+    get '/new_user', to: 'home#new'
+  end
+
+  constraints subdomain: 'ubb' do
+    get '/dashboard', to: 'home#show'
+    get '/new_user', to: 'home#new'
+  end
+
 end
